4cde0ee6696804d3290a40da041f53625d7ab655
Hustini <ejdersalih02@gmail.com>
2024-08-28 11:55:38 +0200

    added placeholder code for this component

---
+++ app/compontents/ProjectCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const projectCard = () => {
+    return(
+        <div className="flex item-center justify-center bg-violet-100">
+            <p className="p-4">Space for projects with dialog</p>
+        </div>
+    )
+}
+
+export default projectCard
