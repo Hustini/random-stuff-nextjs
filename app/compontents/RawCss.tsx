e0ea2a6bdb0489a09483dbae2482344b382eece9
Hustini <ejdersalih02@gmail.com>
2024-08-28 09:21:16 +0200

    added this img

---
+++ app/compontents/RawCss.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const rawCss = () => {
+    return (
+        <div>
+            <img src="../img/games.jpeg" alt="games" />
+        </div>
+    );
+}
+
+export default rawCss
