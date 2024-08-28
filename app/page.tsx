ae6c4ca325ce21744d35a4ae613d435a58fe5232
Hustini <ejdersalih02@gmail.com>
2024-08-28 15:13:21 +0200

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "./compontents/NavBar"
 import RawCss from '@/app/compontents/RawCss';
 import ProjectCard from '@/app/compontents/ProjectCard';
+import Footer from '@/app/compontents/footer';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <h1 className='text-center text-2xl text-gray-800 m-10 px-2.5'>Welcome to my experimental playground! Here, I test out random ideas, explore new features, and experiment with Next.js. Feel free to browse around and see what I am working on. Enjoy the journey!</h1>
         <RawCss />
         <ProjectCard />
+        <Footer />
     </main>
   );
 }
