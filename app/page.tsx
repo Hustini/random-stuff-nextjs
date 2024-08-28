39db13e6eb6215e1cfe3ef65158cf1d078d66d9b
Hustini <ejdersalih02@gmail.com>
2024-08-28 08:42:10 +0200

    added a little text which explain this project

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1 className='text-center text-2xl text-gray-800 m-10 px-2.5'>Welcome to my experimental playground! Here, I test out random ideas, explore new features, and experiment with Next.js. Feel free to browse around and see what I am working on. Enjoy the journey!</h1>
     </main>
   );
 }
