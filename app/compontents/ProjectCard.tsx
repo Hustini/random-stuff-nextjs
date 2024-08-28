e3f70948288d087a036d6b388773bf333a97772c
Hustini <ejdersalih02@gmail.com>
2024-08-28 14:56:41 +0200

    changed the text

--- app/compontents/ProjectCard.tsx
+++ app/compontents/ProjectCard.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const projectCard = () => {
     return(
         <div className="flex item-center justify-center bg-violet-100 m-4 rounded-[20px]">
-            <p className="p-4">Space for projects with dialog</p>
+            <p className="p-4">Space for projects with modal dialog</p>
         </div>
     )
 }
