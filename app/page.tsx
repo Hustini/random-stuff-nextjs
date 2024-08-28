d2e65baa6a004f252b14263d54623784d8a99e65
Hustini <ejdersalih02@gmail.com>
2024-08-28 11:07:05 +0200

    add a rawcss component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import NavBar from "./compontents/NavBar"
+import RawCss from '@/app/compontents/RawCss';
 
 export default function Home() {
   return (
     <main>
-      <h1 className='text-center text-2xl text-gray-800 m-10 px-2.5'>Welcome to my experimental playground! Here, I test out random ideas, explore new features, and experiment with Next.js. Feel free to browse around and see what I am working on. Enjoy the journey!</h1>
+        <h1 className='text-center text-2xl text-gray-800 m-10 px-2.5'>Welcome to my experimental playground! Here, I test out random ideas, explore new features, and experiment with Next.js. Feel free to browse around and see what I am working on. Enjoy the journey!</h1>
+        <RawCss />
     </main>
   );
 }
