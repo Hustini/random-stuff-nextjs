4ce593d322e3c6d405896367fd2d888b689013c5
Hustini <ejdersalih02@gmail.com>
2024-08-28 12:59:02 +0200

    added margin and rounded the edges

--- app/compontents/ProjectCard.tsx
+++ app/compontents/ProjectCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const projectCard = () => {
     return(
-        <div className="flex item-center justify-center bg-violet-100">
+        <div className="flex item-center justify-center bg-violet-100 m-4 rounded-[20px]">
             <p className="p-4">Space for projects with dialog</p>
         </div>
     )
