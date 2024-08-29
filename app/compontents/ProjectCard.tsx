74f17def1fd0fd40068d7a138d9a8fc8464cd495
Hustini <ejdersalih02@gmail.com>
2024-08-29 11:58:30 +0200

    modal now looks right

--- app/compontents/ProjectCard.tsx
+++ app/compontents/ProjectCard.tsx
@@ -13,7 +13,7 @@ const ProjectsCard: React.FC = () => {
     return (
         <>
             <button className="btn bg-violet-100 p-4 rounded-[20px] hover:bg-violet-300" onClick={openModal}>Open Modal</button>
-            <dialog id="my_modal_3" className="modal lg:w-2/3 lg:h-3/7">
+            <dialog id="my_modal_3" className="modal w-2/3 h-3/7">
                 <div className="modal-box rounded-[20px] p-4">
                     <form method="dialog">
                         {/* If there is a button in form, it will close the modal */}
@@ -21,11 +21,11 @@ const ProjectsCard: React.FC = () => {
                             className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
                             type="submit">✕</button>
                     </form>
-                    <div className="flex flex-col lg:flex-row">
-                        <div className="flex justify-center lg:justify-start">
-                            <Image src={"/img/games.png"} alt={"this is a placeholder"} width={200} height={200} className="w-1/2 lg:w-auto" />
+                    <div className="flex flex-row">
+                        <div className="flex justify-center flex-shrink-0">
+                            <Image src={"/img/games.png"} alt={"this is a placeholder"} width={200} height={200} />
                         </div>
-                        <div className="pl-4 mt-4 lg:mt-0">
+                        <div className="pl-4">
                             <h3 className="font-bold text-lg">Hello!</h3>
                             <p className="py-4 text-justify">
                                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
