71dc5f227ef86d59544b87d99fee67aef73aa1e8
Hustini <ejdersalih02@gmail.com>
2024-08-30 10:06:54 +0200

    changed the flex direction and added more project cards. they also work with parameters now

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,13 @@ export default function Home() {
         <NavBar />
         <h1 className='text-center text-2xl text-gray-800 m-10 px-2.5'>Welcome to my experimental playground! Here, I test out random ideas, explore new features, and experiment with Next.js. Feel free to browse around and see what I am working on. Enjoy the journey!</h1>
         <RawCss />
-        <div className="flex flex-row items-center justify-center w-full m-4 gap-4">
+        <div className="flex flex-wrap items-center justify-center w-full mb-4 gap-4">
             <ProjectCard buttonText={"Project 1"} />
-            <ProjectCard buttonText={"Project 2"}/>
+            <ProjectCard buttonText={"Project 2"} />
+            <ProjectCard buttonText={"Project 3"} />
+            <ProjectCard buttonText={"Project 4"} />
+            <ProjectCard buttonText={"Project 5"} />
+            <ProjectCard buttonText={"Project 6"} />
         </div>
         <Footer />
     </main>
